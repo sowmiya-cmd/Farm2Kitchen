2624e75f199bab17bbf974689a255b642d772bdb
Sowmiya,TCE,Madurai <61686122+sowmiya-cmd@users.noreply.github.com>
2020-10-13 18:05:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
 'use strict';
 
-// App Module: the name AngularStore matches the ng-app attribute in the main <html> tag
-// the route provides parses the URL and injects the appropriate partial page
 var storeApp = angular.module('AngularStore', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/store', {
-        templateUrl: 'partials/store.htm',
+        templateUrl: 'partials/store.html',
         controller: storeController 
       }).
       when('/products/:productSku', {
-        templateUrl: 'partials/product.htm',
+        templateUrl: 'partials/product.html',
         controller: storeController
       }).
       when('/cart', {
-        templateUrl: 'partials/shoppingCart.htm',
+        templateUrl: 'partials/shoppingCart.html',
         controller: storeController
       }).
       otherwise({
@@ -22,28 +20,19 @@ var storeApp = angular.module('AngularStore', []).
       });
 }]);
 
-// create a data service that provides a store and a shopping cart that
-// will be shared by all views (instead of creating fresh ones for each view).
+
 storeApp.factory("DataService", function () {
 
-    // create store
+  
     var myStore = new store();
 
-    // create shopping cart
+   
     var myCart = new shoppingCart("AngularStore");
 
-    // enable PayPal checkout
-    // note: the second parameter identifies the merchant; in order to use the 
-    // shopping cart with PayPal, you have to create a merchant account with 
-    // PayPal. You can do that here:
-    // https://www.paypal.com/webapps/mpp/merchant
+    
     myCart.addCheckoutParameters("PayPal", "paypaluser@youremail.com");
 
-    // enable Google Wallet checkout
-    // note: the second parameter identifies the merchant; in order to use the 
-    // shopping cart with Google Wallet, you have to create a merchant account with 
-    // Google. You can do that here:
-    // https://developers.google.com/commerce/wallet/digital/training/getting-started/merchant-setup
+   
     myCart.addCheckoutParameters("Google", "xxxxxxx",
         {
             ship_method_name_1: "UPS Next Day Air",
@@ -55,18 +44,14 @@ storeApp.factory("DataService", function () {
         }
     );
 
-    // enable Stripe checkout
-    // note: the second parameter identifies your publishable key; in order to use the 
-    // shopping cart with Stripe, you have to create a merchant account with 
-    // Stripe. You can do that here:
-    // https://manage.stripe.com/register
+  
     myCart.addCheckoutParameters("Stripe", "pk_test_xxxx",
         {
             chargeurl: "https://localhost:1234/processStripe.aspx"
         }
     );
 
-    // return data object with store and cart
+   
     return {
         store: myStore,
         cart: myCart
